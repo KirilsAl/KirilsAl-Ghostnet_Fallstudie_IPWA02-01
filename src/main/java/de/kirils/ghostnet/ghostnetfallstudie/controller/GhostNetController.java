d2461c491bc8451df92ba6a017bce813652ea97a
Kirils Aleksejevs <aleksejevs2003@gmail.com>
2025-11-19 23:06:48 +0100

    Backend fertiggestellt

--- src/main/java/de/kirils/ghostnet/ghostnetfallstudie/controller/GhostNetController.java
+++ src/main/java/de/kirils/ghostnet/ghostnetfallstudie/controller/GhostNetController.java
@@ -9,6 +9,9 @@ import de.kirils.ghostnet.ghostnetfallstudie.repo.GhostNetRepository;
 import de.kirils.ghostnet.ghostnetfallstudie.repo.PersonRepository;
 import de.kirils.ghostnet.ghostnetfallstudie.service.GhostNetService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -54,5 +57,44 @@ public class GhostNetController {
                 NetStatus.BERGUNG_BEVORSTEHEND
         );
         return ghostNetRepository.findByNetStatusIn((statuses));
+    }
+
+    @PostMapping("/{netId}/assign/{personId}")
+    public GhostNet assignBergendePerson(@PathVariable Long netId,
+                                         @PathVariable Long personId) {
+        GhostNet ghostNet = ghostNetRepository.findById(netId).orElseThrow(() -> new RuntimeException("GhostNet mit ID" + netId + " nicht gefunden"));
+
+        Person person = personRepository.findById(personId).orElseThrow(() -> new RuntimeException("Person mit der ID" + personId + " nicht gefunden"));
+
+        person.setpersonRole(PersonRole.BERGEND);
+        personRepository.save(person);
+
+        ghostNet.setbergendePersonID(person);
+
+        ghostNet.setNetStatus(NetStatus.GEMELDET);
+
+        return ghostNetRepository.save(ghostNet);
+    }
+
+    @PostMapping("/{netId/complete")
+    public GhostNet markNetAsComplete(@PathVariable Long netId) {
+        GhostNet ghostNet = ghostNetRepository.findById(netId).orElseThrow(() -> new RuntimeException("GhostNet mit der ID " +  netId + " nicht gefunden"));
+
+        if (ghostNet.getBergendePersonID() == null) {
+            throw new RuntimeException("Netz " + netId + "kann nicht als geborgen markiert werden, keine bergende Person eingetragen");
+        }
+
+        ghostNet.setNetStatus(NetStatus.GEMELDET);
+
+        return ghostNetRepository.save(ghostNet);
+    }
+
+    @PostMapping("/{netId}/lost")
+    public GhostNet markNetAsLost(@PathVariable Long netId) {
+        GhostNet ghostNet = ghostNetRepository.findById(netId).orElseThrow(() -> new RuntimeException("Netz " + netId + " nicht gefunden"));
+
+        ghostNet.setNetStatus(NetStatus.VERSCHOLLEN);
+
+        return ghostNetRepository.save(ghostNet);
     }
 }
