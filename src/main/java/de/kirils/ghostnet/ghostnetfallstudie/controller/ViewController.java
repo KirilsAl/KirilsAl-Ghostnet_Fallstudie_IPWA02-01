e78bb92b4cb2ad2c1da104043a8992b29902bc0b
Kirils Aleksejevs <aleksejevs2003@gmail.com>
2025-11-24 20:35:03 +0100

    Web Oberfläche angefangen. ViewController angelegt. Netze kann man jetzt anlegen

--- src/main/java/de/kirils/ghostnet/ghostnetfallstudie/controller/ViewController.java
+++ src/main/java/de/kirils/ghostnet/ghostnetfallstudie/controller/ViewController.java
@@ -1,4 +1,83 @@
 package de.kirils.ghostnet.ghostnetfallstudie.controller;
 
+import de.kirils.ghostnet.ghostnetfallstudie.dto.ReportNetForm;
+import de.kirils.ghostnet.ghostnetfallstudie.model.GhostNet;
+import de.kirils.ghostnet.ghostnetfallstudie.model.NetStatus;
+import de.kirils.ghostnet.ghostnetfallstudie.model.Person;
+import de.kirils.ghostnet.ghostnetfallstudie.model.PersonRole;
+import de.kirils.ghostnet.ghostnetfallstudie.repo.GhostNetRepository;
+import de.kirils.ghostnet.ghostnetfallstudie.repo.PersonRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
 public class ViewController {
+
+    private final GhostNetRepository ghostNetRepository;
+    private final PersonRepository personRepository;
+
+    public ViewController(GhostNetRepository ghostNetRepository,
+                          PersonRepository personRepository) {
+        this.ghostNetRepository = ghostNetRepository;
+        this.personRepository = personRepository;
+    }
+
+    // Startseite: einfach die Liste der Netze
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("nets", ghostNetRepository.findAll());
+        return "nets/list";
+    }
+
+    // Liste aller Netze anzeigen
+    @GetMapping("/nets")
+    public String showNetList(Model model) {
+        model.addAttribute("nets", ghostNetRepository.findAll());
+        return "nets/list";
+    }
+
+    // Formular anzeigen
+    @GetMapping("/nets/create")
+    public String showCreateNetForm(Model model) {
+        model.addAttribute("reportNetForm", new ReportNetForm());
+        return "nets/create";
+    }
+
+    // Formular verarbeiten
+    @PostMapping("/nets/create")
+    public String handleCreateNetForm(
+            @ModelAttribute("reportNetForm") ReportNetForm form
+    ) {
+        System.out.println("FORM SUBMITTED: " + form.getGpsLat() + " / " + form.getGpsLon());
+
+        Person meldendePerson = null;
+
+        if (form.getName() != null && !form.getName().isBlank()) {
+            meldendePerson = new Person(
+                    form.getName(),
+                    form.getPhone(),
+                    PersonRole.MELDEND
+            );
+            personRepository.save(meldendePerson);
+        }
+
+        GhostNet ghostNet = new GhostNet(
+                form.getGpsLat(),
+                form.getGpsLon(),
+                form.getSizeEstimate(),
+                LocalDateTime.now(),
+                null,
+                NetStatus.GEMELDET
+        );
+
+        ghostNetRepository.save(ghostNet);
+
+        return "redirect:/nets";
+
+    }
 }
--- src/main/java/de/kirils/ghostnet/ghostnetfallstudie/dto/ReportNetForm.java
+++ src/main/java/de/kirils/ghostnet/ghostnetfallstudie/dto/ReportNetForm.java
@@ -1,4 +1,53 @@
 package de.kirils.ghostnet.ghostnetfallstudie.dto;
 
-public record ReportNetForm() {
+public class ReportNetForm {
+
+    private double gpsLat;
+    private double gpsLon;
+    private double sizeEstimate;
+    private String name;
+    private String phone;
+
+    public ReportNetForm() {
+    }
+
+    public double getGpsLat() {
+        return gpsLat;
+    }
+
+    public void setGpsLat(double gpsLat) {
+        this.gpsLat = gpsLat;
+    }
+
+    public double getGpsLon() {
+        return gpsLon;
+    }
+
+    public void setGpsLon(double gpsLon) {
+        this.gpsLon = gpsLon;
+    }
+
+    public double getSizeEstimate() {
+        return sizeEstimate;
+    }
+
+    public void setSizeEstimate(double sizeEstimate) {
+        this.sizeEstimate = sizeEstimate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
